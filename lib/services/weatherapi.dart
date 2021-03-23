fbe4da8e788f5dc8a0c96197bce91f57c6f41586
juzer-patan <66876971+juzer-patan@users.noreply.github.com>
2021-03-23 21:33:06 +0530

    Update weatherapi.dart

--- lib/services/weatherapi.dart
+++ lib/services/weatherapi.dart
@@ -4,7 +4,7 @@ import 'package:animation_test/services/weatherJson.dart';
 import 'package:http/http.dart' as http;
 class WeatherApi {
   static getWeather(String city) async{
-    String url = "http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=21881a7c4c41ba5473cb5ed70ea92214";
+    String url = "http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=";
     var response = await http.get(url);
     
     return Weather.fromJson(jsonDecode(response.body));
